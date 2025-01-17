9e5195843c3fbe3bdccdd14ff3594ef25c658257
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-01-17 21:52:42 +0530

    company name added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,9 +8,9 @@ const LandingPage = () => {
     <div className="landing-page">
     <div className="background"></div>
     <div className="content">
-      <h1>WELCOME TO PLANT NURSERY</h1>
+      <h1>WELCOME TO GREEN HAVEN</h1>
       <p className="about-us">
-          At Plant Nursery, we are passionate about transforming spaces into lush, green environments that inspire and rejuvenate. From tiny succulents to towering indoor plants, we offer a curated selection to suit every taste and setting. Our commitment goes beyond just selling plants—we're here to guide you in creating a sustainable and thriving indoor garden.
+          At Green Haven, we are passionate about transforming spaces into lush, green environments that inspire and rejuvenate. From tiny succulents to towering indoor plants, we offer a curated selection to suit every taste and setting. Our commitment goes beyond just selling plants—we're here to guide you in creating a sustainable and thriving indoor garden.
         </p>
       <Link to="/plants">
         <button className='get-started-button'>Get Started</button>
